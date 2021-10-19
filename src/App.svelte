37fc4f5aa7a05f5ff96102d0281eda71e0f5f6d9
Ren, Yi <yi.ren@ADP.com>
2021-10-19 19:42:24 -0400

    :tada: begin

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Chat from './Chat.svelte';
+  import Header from './Header.svelte';
+import Login from './Login.svelte';
+</script>
+
+<div class="app">
+    <Header />
+    <Chat />
+</div>
